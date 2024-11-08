2754b525175c4cda0584befd846f4c6b8d74ab88
4C554D43 <151928410+4C554D43@users.noreply.github.com>
2024-11-08 14:12:17 +0100

    Update formatting.R

--- R/formatting.R
+++ R/formatting.R
@@ -27,6 +27,9 @@ format_tree <- function(tr){
   # Function -------------------------------------------------------------------
   
   cat("debug 1 ->",  1:ape::Nnode(tr))
+  ape::Nnode(tr) <- 29\
+  cat("debug 1 ->",  1:ape::Nnode(tr))
+  
   for (i in 1:ape::Nnode(tr)) {
   cat("debug: ", i)
     
